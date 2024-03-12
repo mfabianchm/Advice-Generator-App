a7d9b3e5e21bc777666aae3d6bd7f05681675801
mfabianchm <mfchongm@hotmail.com>
2024-03-12 12:20:23 -0600

    Initial Commit

---
+++ Advice-Generator App/Advice_Generator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advice_Generator_AppApp.swift
+//  Advice-Generator App
+//
+//  Created by Marcos Fabian Chong Megchun on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Advice_Generator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
